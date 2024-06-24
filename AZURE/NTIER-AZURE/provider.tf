0e6f751705fe6dfdea555af209b27c874d9026a3
UrvashiHarode1432 <urvashiharode1432@gmail.com>
2024-06-24 16:25:36 +0530

    version constraints added

---
+++ AZURE/NTIER-AZURE/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    #azurerm version constraints
+    azurerm = {
+        source  = "hashicorp/azurerm"
+        version = "~>=3.0.0"
+    }
+  }
+  #terraform version constraints
+  required_version = ">= 1.5.0"
+}
+
+provider "azurerm" {
+  features {
+    
+  }
+}

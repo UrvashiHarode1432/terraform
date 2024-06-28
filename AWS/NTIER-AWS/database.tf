6d7a12b04d8ff4868938bebb7b667239d75f2ff2
UrvashiHarode1432 <urvashiharode1432@gmail.com>
2024-06-28 15:29:44 +0530

    added RDS instance / database in aws

--- AWS/NTIER-AWS/database.tf
+++ AWS/NTIER-AWS/database.tf
@@ -6,16 +6,28 @@ data "aws_subnets" "db" {
   depends_on = [aws_subnet.subnets]
 }
 
-resource "aws_db_subnet_group" "ntier_db" {
-  name       = "ntier_db"
+resource "aws_db_subnet_group" "ntier_subnet_group" {
+  name       = "ntier_subnet_group"
   subnet_ids = data.aws_subnets.db.ids
   #   subnet_ids = [aws_subnet.subnets[4].id, aws_subnet.subnets[5].id]
   tags = {
-    Name = "ntier_db"
+    Name = "ntier_subnet_group"
   }
   depends_on = [aws_subnet.subnets, data.aws_subnets.db]
 }
 
-# resource "aws_db_instance" "ntierdb" {
+resource "aws_db_instance" "ntier_db" {
+  allocated_storage      = 10
+  db_name                = "emp"
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
+  username               = "root"
+  password               = "rootroot"
+  skip_final_snapshot    = true
+  db_subnet_group_name   = aws_db_subnet_group.ntier_subnet_group.name
+  identifier             = "emp-rds"
+  vpc_security_group_ids = [aws_security_group.dbsg.id]
 
-# }
+  depends_on = [aws_db_subnet_group.ntier_subnet_group, aws_vpc.ntier_vpc, aws_subnet.subnets]
+}
